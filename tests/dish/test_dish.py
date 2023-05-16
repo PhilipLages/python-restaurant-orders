15cf12e71f0ab5bbe7ae3d2c5fd4f4eac33da071
PhilipLages <psilvaelages@gmail.com>
2023-05-16 20:51:14 -0300

    refactor: Refactors test_ingredient to add fixtures

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,55 @@
-from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.dish import Dish, Ingredient
+import pytest
+
+
+@pytest.fixture
+def ingredient():
+    return Ingredient("queijo mussarela")
 
 
 # Req 2
-def test_dish():
-    pass
+def test_dish(ingredient):
+    # Test instance creation with valid price
+    dish = Dish("Pizza Margherita", 29.99)
+    assert dish.name == "Pizza Margherita"
+    assert dish.price == 29.99
+    assert dish.recipe == {}
+
+    # Test instance creation with invalid price (not a float)
+    with pytest.raises(TypeError):
+        Dish("Invalid Dish", "price")
+
+    # Test instance creation with invalid price (less than or equal to zero)
+    with pytest.raises(ValueError):
+        Dish("Invalid Dish", 0)
+
+    # Test __repr__ method
+    expected_repr = "Dish('Pizza Margherita', R$29.99)"
+    assert repr(dish) == expected_repr
+
+    # Test __eq__ method with the same dish
+    dish2 = Dish("Pizza Margherita", 29.99)
+    assert dish.__eq__(dish2)
+
+    # Test __eq__ method with different dishes
+    dish3 = Dish("Pepperoni Pizza", 35.99)
+    assert not dish.__eq__(dish3)
+
+    # Test __hash__ method
+    assert hash(dish) == hash(dish2)
+
+    # Test add_ingredient_dependency method
+    dish.add_ingredient_dependency(ingredient, 2)
+    assert dish.recipe == {ingredient: 2}
+
+    # Test get_restrictions method
+    ingredient.add_restriction("Vegan")
+    assert dish.get_restrictions() == {"Vegan"}
+
+    # Test get_ingredients method
+    assert dish.get_ingredients() == {ingredient}
+
+
+if __name__ == '__main__':
+    pytest.main()
+
--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,10 +1,24 @@
-from src.models.ingredient import Ingredient
-from src.models.ingredient import Restriction
+from src.models.ingredient import Ingredient, Restriction
+from pytest import fixture
+
+
+@fixture
+def ingredient1():
+    return Ingredient("queijo mussarela")
+
+
+@fixture
+def ingredient2():
+    return Ingredient("queijo mussarela")
+
+
+@fixture
+def ingredient3():
+    return Ingredient("bacon")
 
 
 # Req 1
-def test_ingredient():
-    ingredient1 = Ingredient("queijo mussarela")
+def test_ingredient(ingredient1, ingredient2, ingredient3):
     assert ingredient1.name == "queijo mussarela"
 
     expected_restrictions = {Restriction.LACTOSE, Restriction.ANIMAL_DERIVED}
@@ -13,11 +27,9 @@ def test_ingredient():
     expected_repr = "Ingredient('queijo mussarela')"
     assert repr(ingredient1) == expected_repr
 
-    ingredient2 = Ingredient("queijo mussarela")
-    assert ingredient1 == ingredient2
+    assert ingredient1.__eq__(ingredient2)
 
-    ingredient3 = Ingredient("bacon")
-    assert ingredient1 != ingredient3
+    assert not ingredient1.__eq__(ingredient3)
 
     assert hash(ingredient1) == hash(ingredient2)
 
